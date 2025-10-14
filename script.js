38dddba0c68c678b180b022ef5f195aa716119ad
Terryisme122 <superterry23@gmail.com>
2025-10-14 19:51:11 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toTop = document.getElementById('toTop');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) { toTop.style.display = 'flex'; }
+  else { toTop.style.display = 'none'; }
+});
+toTop.addEventListener('click', () => window.scrollTo({top:0, behavior:'smooth'}));
